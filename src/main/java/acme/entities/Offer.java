838886a69f0f955e84358e9b0dd83897338a2032
manortbla <manortbla@alum.us.es>
2023-02-22 17:47:50 +0100

    Entidad Offer creada Fixes #7

---
+++ src/main/java/acme/entities/Offer.java
@@ -0,0 +1,57 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Offer extends AbstractEntity {
+
+	// Serialisation identifier 
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes
+	@PastOrPresent
+	protected Date				instantiation;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			heading;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			summary;
+
+	@Temporal(TemporalType.DATE)
+	@NotNull
+	@FutureOrPresent
+	protected Date				availabilityPeriod;
+
+	@PositiveOrZero
+	protected Double			price;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
