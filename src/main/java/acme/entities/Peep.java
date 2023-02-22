9437786a383cd79e8119a23f3997da753f18b2e0
pedlopruz <plopezr2011@gmail.com>
2023-02-22 12:35:42 +0100

    Feat:
    Nuevas entidades tutorial, peep y assistant

---
+++ src/main/java/acme/entities/Peep.java
@@ -0,0 +1,46 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Peep extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@PastOrPresent
+	protected Date				instantiation;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			nick;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			message;
+
+	@Email
+	protected String			email;
+
+	@URL
+	protected String			link;
+
+}
