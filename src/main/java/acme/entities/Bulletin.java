7a5b8cb7757670a07cbf36c722af0eef18b01a44
mannavsic <mannavsic@alum.us.es>
2023-02-22 17:50:20 +0100

    Entidad Bulletin creada Fixes #6

---
+++ src/main/java/acme/entities/Bulletin.java
@@ -0,0 +1,51 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Bulletin extends AbstractEntity {
+
+	// Serialisation identifier 
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				moment;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			text;
+
+	protected boolean			flag;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
