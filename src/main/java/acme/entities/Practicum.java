1ebfd74987e74049325a1281b7c7bf8b4013195d
manortbla <manortbla@alum.us.es>
2023-03-01 11:32:38 +0100

    Entidad Practicum creada Fixes #19

---
+++ src/main/java/acme/entities/Practicum.java
@@ -0,0 +1,43 @@
+
+package acme.entities;
+
+import java.awt.geom.Arc2D.Double;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Practicum extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}", message = "{validation.tutorial.code}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstracts;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+	protected Double			estimatedTime;
+
+}
