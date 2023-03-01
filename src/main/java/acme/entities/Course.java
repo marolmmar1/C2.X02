7aaaea76fd6affd69e5f05092fe79b074a9b6110
mannavsic <mannavsic@alum.us.es>
2023-03-01 20:12:59 +0100

    Entidad Course creada Fixes #24

---
+++ src/main/java/acme/entities/Course.java
@@ -0,0 +1,42 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.components.datatypes.Money;
+
+public class Course {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}", message = "{validation.course.code}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstracts;
+
+	@NotNull
+	protected Money				price;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	protected CourseType		courseType;
+
+	@URL
+	protected String			link;
+}
