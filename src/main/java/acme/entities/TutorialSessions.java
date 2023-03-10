eee81bcccb5fd59edcaa877082df50783d414b13
pedlopruz <plopezr2011@gmail.com>
2023-03-10 12:21:53 +0100

    Feat:
    Relacion TutorialSessions y creacion Tutorial sessions.csv

--- src/main/java/acme/entities/TutorialSessions.java
+++ src/main/java/acme/entities/TutorialSessions.java
@@ -6,8 +6,10 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -47,5 +49,12 @@ public class TutorialSessions extends AbstractEntity {
 
 	@URL
 	protected String			link;
+
+	protected boolean			draftMode;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Tutorial			tutorial;
 
 }
