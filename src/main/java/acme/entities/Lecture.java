b1eb7499b6781886e1e6eb4bdd0f4902b35c6605
pedlopruz <plopezr2011@gmail.com>
2023-03-14 13:11:15 +0100

    Error estimatedTime

--- src/main/java/acme/entities/Lecture.java
+++ src/main/java/acme/entities/Lecture.java
@@ -4,6 +4,7 @@ package acme.entities;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -30,8 +31,8 @@ public class Lecture extends AbstractEntity {
 	@Length(max = 100)
 	protected String			abstracts;
 
-	//	@Digits(integer = 3, fraction = 2)
-	//	protected Double			estimatedTime;
+	@Digits(integer = 3, fraction = 2)
+	protected Double			estimatedTime;
 
 	@NotBlank
 	@Length(max = 100)
