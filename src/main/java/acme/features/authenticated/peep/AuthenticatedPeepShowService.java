01cc353e948db4d13efbb872f5d42b7f954636c1
Carolina_Carrasco <carcardia2@alum.us.es>
2023-04-11 11:58:54 +0200

    List and show Peeps

---
+++ src/main/java/acme/features/authenticated/peep/AuthenticatedPeepShowService.java
@@ -0,0 +1,71 @@
+/*
+ * AuthenticatedAnnouncementShowService.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.authenticated.peep;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Peep;
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AuthenticatedPeepShowService extends AbstractService<Authenticated, Peep> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuthenticatedPeepRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Peep object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOnePeepById(id);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void unbind(final Peep object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "instantiation", "title", "nick", "message", "email", "link");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
