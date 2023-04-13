c4bc47f90aff2cf6f235833eabf16af2c7eeed7e
Angel Lorenzo Casas <angelorenzo63@hotmail.com>
2023-04-13 17:35:00 +0200

    added banner randomizer(does not appear yet)

--- src/main/java/acme/entities/Banner.java
+++ src/main/java/acme/entities/Banner.java
@@ -14,6 +14,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
+import acme.framework.helpers.MomentHelper;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -48,5 +49,17 @@ public class Banner extends AbstractEntity {
 	@URL
 	@NotNull
 	protected String			linkImage;
+
+
+	public Boolean isActive() {
+		boolean active = false;
+		final Date currentMoment = MomentHelper.getCurrentMoment();
+		if (currentMoment.after(this.inicialPeriod) && currentMoment.before(this.finalPeriod))
+			active = true;
+		else
+			active = false;
+
+		return active;
+	}
 
 }
