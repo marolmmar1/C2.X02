b7f5f89cfc3d273bae7514b1294c9f9f741063ca
Angel Lorenzo Casas <angelorenzo63@hotmail.com>
2023-05-21 14:04:56 +0200

    test of audit and auditingRecord finished

---
+++ src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordListTest.java
@@ -0,0 +1,109 @@
+/*
+ * EmployerDutyListTest.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.auditor.auditingRecord;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.Audit;
+import acme.testing.TestHarness;
+
+class AuditorAuditingRecordListTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuditorAuditingRecordTestRepository repository;
+
+	// Test methods -----------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/auditing-record/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int auditRecordIndex, final String code, final int auditingRecordRecordIndex, final String subject) {
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "Audit List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(auditRecordIndex, 0, code);
+		super.clickOnListingRecord(auditRecordIndex);
+		super.checkInputBoxHasValue("code", code);
+		super.clickOnButton("Audit Records");
+
+		super.checkListingExists();
+		super.checkColumnHasValue(auditingRecordRecordIndex, 0, subject);
+		super.clickOnListingRecord(auditingRecordRecordIndex);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test200Negative() {
+		// HINT: there's no negative test case for this listing, since it doesn't
+		// HINT+ involve filling in any forms.
+	}
+
+	@Test
+	public void test300Hacking() {
+		Collection<Audit> audits;
+		String param;
+
+		audits = this.repository.findManyAuditsByAuditorUsername("auditor1");
+		for (final Audit audit : audits)
+			if (audit.isDraftMode()) {
+				param = String.format("id=%d", audit.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+
+				super.signIn("administrator", "administrator");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("auditor2", "auditor2");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("lecturer1", "lecturer1");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("student1", "student1");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("company1", "company1");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("assistant1", "assistant1");
+				super.request("/auditor/auditing-record/list", param);
+				super.checkPanicExists();
+				super.signOut();
+			}
+	}
+
+}
--- src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordUpdateTest.java
+++ src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordUpdateTest.java
@@ -1,5 +1,5 @@
 /*
- * EmployerDutyCreateTest.java
+ * EmployerJobUpdateTest.java
  *
  * Copyright (C) 2012-2023 Rafael Corchuelo.
  *
@@ -22,21 +22,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import acme.entities.Audit;
 import acme.testing.TestHarness;
 
-class AuditorAuditingRecordCreateTest extends TestHarness {
+public class AuditorAuditingRecordUpdateTest extends TestHarness {
 
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
 	protected AuditorAuditingRecordTestRepository repository;
 
-	// Test methods -----------------------------------------------------------
+	// Test methods ------------------------------------------------------------
 
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/auditor/auditing-record/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/auditor/auditing-record/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test100Positive(final int auditRecordIndex, final int auditingRecordRecordIndex, final String subject, final String assessment, final String markType, final String initialPeriod, final String finalPeriod, final String link) {
 
-		super.signIn("auditor15", "auditor15");
+		super.signIn("auditor17", "auditor17");
 
 		super.clickOnMenu("Auditor", "Audit List");
 		super.checkListingExists();
@@ -45,34 +45,38 @@ class AuditorAuditingRecordCreateTest extends TestHarness {
 		super.clickOnListingRecord(auditRecordIndex);
 		super.clickOnButton("Audit Records");
 
-		super.clickOnButton("Create");
+		super.clickOnListingRecord(auditingRecordRecordIndex);
+		super.checkFormExists();
+
 		super.fillInputBoxIn("subject", subject);
 		super.fillInputBoxIn("assessment", assessment);
 		super.fillInputBoxIn("markType", markType);
 		super.fillInputBoxIn("initialPeriod", initialPeriod);
 		super.fillInputBoxIn("finalPeriod", finalPeriod);
 		super.fillInputBoxIn("link", link);
-		super.clickOnSubmit("Create");
+		super.clickOnSubmit("Update");
 
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		super.checkColumnHasValue(auditingRecordRecordIndex, 0, subject);
 
 		super.clickOnListingRecord(auditingRecordRecordIndex);
+		super.checkFormExists();
 		super.checkInputBoxHasValue("subject", subject);
 		super.checkInputBoxHasValue("assessment", assessment);
 		super.checkInputBoxHasValue("markType", markType);
 		super.checkInputBoxHasValue("initialPeriod", initialPeriod);
 		super.checkInputBoxHasValue("finalPeriod", finalPeriod);
 		super.checkInputBoxHasValue("link", link);
+
 		super.signOut();
 	}
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/auditor/auditing-record/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/auditor/auditing-record/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void test200Negative(final int auditRecordIndex, final int auditingRecordRecordIndex, final String subject, final String assessment, final String markType, final String initialPeriod, final String finalPeriod, final String link) {
 
-		super.signIn("auditor15", "auditor15");
+		super.signIn("auditor17", "auditor17");
 
 		super.clickOnMenu("Auditor", "Audit List");
 		super.checkListingExists();
@@ -81,14 +85,18 @@ class AuditorAuditingRecordCreateTest extends TestHarness {
 		super.clickOnListingRecord(auditRecordIndex);
 		super.clickOnButton("Audit Records");
 
-		super.clickOnButton("Create");
+		super.clickOnListingRecord(auditingRecordRecordIndex);
+		super.checkFormExists();
+
 		super.fillInputBoxIn("subject", subject);
 		super.fillInputBoxIn("assessment", assessment);
 		super.fillInputBoxIn("markType", markType);
 		super.fillInputBoxIn("initialPeriod", initialPeriod);
 		super.fillInputBoxIn("finalPeriod", finalPeriod);
 		super.fillInputBoxIn("link", link);
-		super.clickOnSubmit("Create");
+
+		super.clickOnSubmit("Update");
+
 		super.checkErrorsExist();
 
 		super.signOut();
@@ -96,75 +104,54 @@ class AuditorAuditingRecordCreateTest extends TestHarness {
 
 	@Test
 	public void test300Hacking() {
+		// HINT: this test tries to update a job with a role other than "Employer",
+		// HINT+ or using an employer who is not the owner.
 
 		final Collection<Audit> audits;
 		String param;
 
 		audits = this.repository.findManyAuditsByAuditorUsername("auditor1");
 		for (final Audit audit : audits) {
-			param = String.format("auditId=%d", audit.getId());
+			param = String.format("id=%d", audit.getId());
 
 			super.checkLinkExists("Sign in");
-			super.request("/auditor/auditing-record/create", param);
+			super.request("/auditor/auditing-record/update", param);
 			super.checkPanicExists();
 
 			super.signIn("administrator", "administrator");
-			super.request("/auditor/auditing-record/create", param);
+			super.request("/auditor/auditing-record/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("auditor2", "auditor2");
+			super.request("/auditor/auditing-record/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("auditor1", "auditor1");
+			super.request("/auditor/auditing-record/update", param);
 			super.checkPanicExists();
 			super.signOut();
 
 			super.signIn("lecturer1", "lecturer1");
-			super.request("/auditor/auditing-record/create", param);
+			super.request("/auditor/auditing-record/update", param);
 			super.checkPanicExists();
 			super.signOut();
 
 			super.signIn("student1", "student1");
-			super.request("/auditor/auditing-record/create", param);
+			super.request("/auditor/auditing-record/update", param);
 			super.checkPanicExists();
 			super.signOut();
 
 			super.signIn("company1", "company1");
-			super.request("/auditor/auditing-record/create", param);
+			super.request("/auditor/auditing-record/update", param);
 			super.checkPanicExists();
 			super.signOut();
 
-			super.signIn("assistant1", "assistant1");
-			super.request("/auditor/auditing-record/create", param);
+			super.signIn("auditor1", "auditor1");
+			super.request("/auditor/auditing-record/update", param);
 			super.checkPanicExists();
 			super.signOut();
-		}
-	}
-
-	@Test
-	public void test301Hacking() {
-
-		final Collection<Audit> audits;
-		String param;
-
-		super.checkLinkExists("Sign in");
-		super.signIn("auditor14", "auditor14");
-		audits = this.repository.findManyAuditsByAuditorUsername("auditor14");
-		for (final Audit audit : audits)
-			if (!audit.isDraftMode()) {
-				param = String.format("auditId=%d", audit.getId());
-				super.request("/auditor/auditing-record/create", param);
-				super.checkPanicExists();
-			}
-	}
-
-	@Test
-	public void test302Hacking() {
-
-		final Collection<Audit> audits;
-		String param;
-
-		super.checkLinkExists("Sign in");
-		super.signIn("auditor11", "auditor11");
-		audits = this.repository.findManyAuditsByAuditorUsername("auditor14");
-		for (final Audit audit : audits) {
-			param = String.format("auditId=%d", audit.getId());
-			super.request("/auditor/auditing-record/create", param);
-			super.checkPanicExists();
 		}
 	}
 
