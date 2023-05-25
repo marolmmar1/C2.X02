8b7c02c3efcf74c41f502ec5dde4d5bbc68153c1
pedlopruz <plopezr2011@gmail.com>
2023-05-25 10:21:12 +0200

    Fix

--- src/test/java/acme/testing/lecturer/courseLecture/LecturerCourseLectureCreateTest.java
+++ src/test/java/acme/testing/lecturer/courseLecture/LecturerCourseLectureCreateTest.java
@@ -1,115 +1,115 @@
-///*
-// * EmployerJobCreateTest.java
-// *
-// * Copyright (C) 2012-2023 Rafael Corchuelo.
-// *
-// * In keeping with the traditional purpose of furthering education and research, it is
-// * the policy of the copyright owner to permit non-commercial use and redistribution of
-// * this software. It has been tested carefully, but it is not guaranteed for any particular
-// * purposes. The copyright owner does not offer any warranties or representations, nor do
-// * they accept any liabilities with respect to them.
-// */
-//
-//package acme.testing.lecturer.courseLecture;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvFileSource;
-//
-//import acme.testing.TestHarness;
-//
-//public class LecturerCourseLectureCreateTest extends TestHarness {
-//
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/lecturer/course-lecture/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	public void test100Positive(final int lectureIndex, final int courseIndex, final String course) {
-//
-//		super.signIn("lecturer17", "lecturer17");
-//
-//		super.clickOnMenu("Lecturer", "List of courses");
-//
-//		super.clickOnListingRecord(courseIndex);
-//		super.clickOnButton("Lectures");
-//		super.checkListingEmpty();
-//
-//		super.clickOnMenu("Lecturer", "List of lectures");
-//
-//		super.checkListingExists();
-//		super.clickOnListingRecord(lectureIndex);
-//		super.clickOnButton("Add to course");
-//		super.fillInputBoxIn("course", course);
-//		super.clickOnSubmit("Add");
-//
-//		super.clickOnMenu("Lecturer", "List of courses");
-//
-//		super.clickOnListingRecord(courseIndex);
-//		super.clickOnButton("Lectures");
-//		super.checkNotListingEmpty();
-//
-//		super.signOut();
-//	}
-//
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/lecturer/course-lecture/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	public void test200Negative(final int lectureIndex, final int courseIndex, final String course) {
-//
-//		super.signIn("lecturer16", "lecturer16");
-//
-//		super.clickOnMenu("Lecturer", "List of courses");
-//
-//		super.clickOnListingRecord(courseIndex);
-//		super.clickOnButton("Lectures");
-//		super.checkNotListingEmpty();
-//
-//		super.clickOnMenu("Lecturer", "List of lectures");
-//
-//		super.checkListingExists();
-//		super.clickOnListingRecord(lectureIndex);
-//		super.clickOnButton("Add to course");
-//		super.fillInputBoxIn("course", course);
-//		super.clickOnSubmit("Add");
-//		super.checkErrorsExist();
-//
-//		super.clickOnMenu("Lecturer", "List of courses");
-//
-//		super.clickOnListingRecord(courseIndex);
-//		super.clickOnButton("Lectures");
-//		super.checkNotListingEmpty();
-//
-//		super.signOut();
-//	}
-//
-//	@Test
-//	public void test300Hacking() {
-//
-//		super.checkLinkExists("Sign in");
-//		super.request("/lecturer/course-lecture/create");
-//		super.checkPanicExists();
-//
-//		super.signIn("administrator", "administrator");
-//		super.request("/lecturer/course-lecture/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//
-//		super.signIn("student1", "student1");
-//		super.request("/lecturer/course-lecture/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//
-//		super.signIn("company1", "company1");
-//		super.request("/lecturer/course-lecture/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//
-//		super.signIn("assistant1", "assistant1");
-//		super.request("/lecturer/course-lecture/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//
-//		super.signIn("auditor1", "auditor1");
-//		super.request("/lecturer/course-lecture/create");
-//		super.checkPanicExists();
-//		super.signOut();
-//	}
-//
-//}
+/*
+ * EmployerJobCreateTest.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.lecturer.courseLecture;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class LecturerCourseLectureCreateTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course-lecture/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int lectureIndex, final int courseIndex, final String course) {
+
+		super.signIn("lecturer17", "lecturer17");
+
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.clickOnListingRecord(courseIndex);
+		super.clickOnButton("Lectures");
+		super.checkListingEmpty();
+
+		super.clickOnMenu("Lecturer", "List of lectures");
+
+		super.checkListingExists();
+		super.clickOnListingRecord(lectureIndex);
+		super.clickOnButton("Add to course");
+		super.fillInputBoxIn("course", course);
+		super.clickOnSubmit("Add");
+
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.clickOnListingRecord(courseIndex);
+		super.clickOnButton("Lectures");
+		super.checkNotListingEmpty();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course-lecture/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int lectureIndex, final int courseIndex, final String course) {
+
+		super.signIn("lecturer16", "lecturer16");
+
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.clickOnListingRecord(courseIndex);
+		super.clickOnButton("Lectures");
+		super.checkNotListingEmpty();
+
+		super.clickOnMenu("Lecturer", "List of lectures");
+
+		super.checkListingExists();
+		super.clickOnListingRecord(lectureIndex);
+		super.clickOnButton("Add to course");
+		super.fillInputBoxIn("course", course);
+		super.clickOnSubmit("Add");
+		super.checkErrorsExist();
+
+		super.clickOnMenu("Lecturer", "List of courses");
+
+		super.clickOnListingRecord(courseIndex);
+		super.clickOnButton("Lectures");
+		super.checkNotListingEmpty();
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+
+		super.checkLinkExists("Sign in");
+		super.request("/lecturer/course-lecture/create");
+		super.checkPanicExists();
+
+		super.signIn("administrator", "administrator");
+		super.request("/lecturer/course-lecture/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("student1", "student1");
+		super.request("/lecturer/course-lecture/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("company1", "company1");
+		super.request("/lecturer/course-lecture/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("assistant1", "assistant1");
+		super.request("/lecturer/course-lecture/create");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("auditor1", "auditor1");
+		super.request("/lecturer/course-lecture/create");
+		super.checkPanicExists();
+		super.signOut();
+	}
+
+}
--- src/test/java/acme/testing/lecturer/lecture/LecturerLectureUpdateTest.java
+++ src/test/java/acme/testing/lecturer/lecture/LecturerLectureUpdateTest.java
@@ -1,144 +1,144 @@
-///*
-// * EmployerJobUpdateTest.java
-// *
-// * Copyright (C) 2012-2023 Rafael Corchuelo.
-// *
-// * In keeping with the traditional purpose of furthering education and research, it is
-// * the policy of the copyright owner to permit non-commercial use and redistribution of
-// * this software. It has been tested carefully, but it is not guaranteed for any particular
-// * purposes. The copyright owner does not offer any warranties or representations, nor do
-// * they accept any liabilities with respect to them.
-// */
-//
-//package acme.testing.lecturer.lecture;
-//
-//import java.util.Collection;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvFileSource;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import acme.entities.Lecture;
-//import acme.testing.TestHarness;
-//
-//public class LecturerLectureUpdateTest extends TestHarness {
-//
-//	// Internal state ---------------------------------------------------------
-//
-//	@Autowired
-//	protected LecturerLectureRepositoryTest repository;
-//
-//	// Test methods ------------------------------------------------------------
-//
-//
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/lecturer/lecture/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	public void test100Positive(final int lectureIndex, final String title, final String abstracts, final String estimatedTime, final String body, final String nature, final String link) {
-//
-//		super.signIn("lecturer10", "lecturer10");
-//
-//		super.clickOnMenu("Lecturer", "List of lectures");
-//		super.checkListingExists();
-//		super.sortListing(0, "asc");
-//
-//		super.clickOnListingRecord(lectureIndex);
-//		super.checkFormExists();
-//		super.fillInputBoxIn("title", title);
-//		super.fillInputBoxIn("abstracts", abstracts);
-//		super.fillInputBoxIn("estimatedTime", estimatedTime);
-//		super.fillInputBoxIn("body", body);
-//		super.fillInputBoxIn("nature", nature);
-//		super.fillInputBoxIn("link", link);
-//		super.clickOnSubmit("Update");
-//
-//		super.checkListingExists();
-//		super.sortListing(0, "asc");
-//		super.checkColumnHasValue(lectureIndex, 0, title);
-//		super.checkColumnHasValue(lectureIndex, 1, abstracts);
-//		super.checkColumnHasValue(lectureIndex, 2, estimatedTime);
-//
-//		super.clickOnListingRecord(lectureIndex);
-//		super.checkFormExists();
-//		super.checkInputBoxHasValue("title", title);
-//		super.checkInputBoxHasValue("abstracts", abstracts);
-//		super.checkInputBoxHasValue("estimatedTime", estimatedTime);
-//		super.checkInputBoxHasValue("body", body);
-//		super.checkInputBoxHasValue("nature", nature);
-//		super.checkInputBoxHasValue("link", link);
-//
-//		super.signOut();
-//	}
-//
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/lecturer/lecture/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	public void test200Negative(final int lectureIndex, final String title, final String abstracts, final String estimatedTime, final String body, final String nature, final String link) {
-//
-//		super.signIn("lecturer10", "lecturer10");
-//
-//		super.clickOnMenu("Lecturer", "List of lectures");
-//		super.checkListingExists();
-//		super.sortListing(0, "asc");
-//
-//		super.clickOnListingRecord(lectureIndex);
-//		super.checkFormExists();
-//		super.fillInputBoxIn("title", title);
-//		super.fillInputBoxIn("abstracts", abstracts);
-//		super.fillInputBoxIn("estimatedTime", estimatedTime);
-//		super.fillInputBoxIn("body", body);
-//		super.fillInputBoxIn("nature", nature);
-//		super.fillInputBoxIn("link", link);
-//		super.clickOnSubmit("Update");
-//
-//		super.checkErrorsExist();
-//
-//		super.signOut();
-//	}
-//
-//	@Test
-//	public void test300Hacking() {
-//
-//		Collection<Lecture> lectures;
-//		String param;
-//
-//		lectures = this.repository.findManyLecturesByLecturerUsername("lecturer10");
-//		for (final Lecture lecture : lectures) {
-//			param = String.format("id=%d", lecture.getId());
-//
-//			super.checkLinkExists("Sign in");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//
-//			super.signIn("administrator", "administrator");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//			super.signOut();
-//
-//			super.signIn("lecturer2", "lecturer2");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//			super.signOut();
-//
-//			super.signIn("student2", "student2");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//			super.signOut();
-//
-//			super.signIn("assistant2", "assistant2");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//			super.signOut();
-//
-//			super.signIn("company2", "company2");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//			super.signOut();
-//
-//			super.signIn("auditor2", "auditor2");
-//			super.request("/lecturer/lecture/update", param);
-//			super.checkPanicExists();
-//			super.signOut();
-//		}
-//	}
-//
-//}
+/*
+ * EmployerJobUpdateTest.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.lecturer.lecture;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.Lecture;
+import acme.testing.TestHarness;
+
+public class LecturerLectureUpdateTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected LecturerLectureRepositoryTest repository;
+
+	// Test methods ------------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/lecture/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int lectureIndex, final String title, final String abstracts, final String estimatedTime, final String body, final String nature, final String link) {
+
+		super.signIn("lecturer10", "lecturer10");
+
+		super.clickOnMenu("Lecturer", "List of lectures");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(lectureIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("abstracts", abstracts);
+		super.fillInputBoxIn("estimatedTime", estimatedTime);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("nature", nature);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Update");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(lectureIndex, 0, title);
+		super.checkColumnHasValue(lectureIndex, 1, abstracts);
+		super.checkColumnHasValue(lectureIndex, 2, estimatedTime);
+
+		super.clickOnListingRecord(lectureIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstracts", abstracts);
+		super.checkInputBoxHasValue("estimatedTime", estimatedTime);
+		super.checkInputBoxHasValue("body", body);
+		super.checkInputBoxHasValue("nature", nature);
+		super.checkInputBoxHasValue("link", link);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/lecture/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int lectureIndex, final String title, final String abstracts, final String estimatedTime, final String body, final String nature, final String link) {
+
+		super.signIn("lecturer10", "lecturer10");
+
+		super.clickOnMenu("Lecturer", "List of lectures");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(lectureIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("abstracts", abstracts);
+		super.fillInputBoxIn("estimatedTime", estimatedTime);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("nature", nature);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Update");
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+
+		Collection<Lecture> lectures;
+		String param;
+
+		lectures = this.repository.findManyLecturesByLecturerUsername("lecturer10");
+		for (final Lecture lecture : lectures) {
+			param = String.format("id=%d", lecture.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator", "administrator");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("lecturer2", "lecturer2");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("student2", "student2");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("assistant2", "assistant2");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("company2", "company2");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("auditor2", "auditor2");
+			super.request("/lecturer/lecture/update", param);
+			super.checkPanicExists();
+			super.signOut();
+		}
+	}
+
+}
