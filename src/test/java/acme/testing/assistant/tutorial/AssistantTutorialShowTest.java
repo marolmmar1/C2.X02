218fa0a976e8eebf29c2768c52ca040a460e9757
pedlopruz <plopezr2011@gmail.com>
2023-05-26 00:24:54 +0200

    Fix: Test Tutorial

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialShowTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialShowTest.java
@@ -1,103 +1,103 @@
-///*
-// * EmployerJobShowTest.java
-// *
-// * Copyright (C) 2012-2023 Rafael Corchuelo.
-// *
-// * In keeping with the traditional purpose of furthering education and research, it is
-// * the policy of the copyright owner to permit non-commercial use and redistribution of
-// * this software. It has been tested carefully, but it is not guaranteed for any particular
-// * purposes. The copyright owner does not offer any warranties or representations, nor do
-// * they accept any liabilities with respect to them.
-// */
-//
-//package acme.testing.assistant.tutorial;
-//
-//import java.util.Collection;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvFileSource;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import acme.entities.Tutorial;
-//import acme.testing.TestHarness;
-//
-//public class AssistantTutorialShowTest extends TestHarness {
-//
-//	// Internal state ---------------------------------------------------------
-//
-//	@Autowired
-//	protected AssistantTutorialTestRepository repository;
-//
-//	// Test data --------------------------------------------------------------
-//
-//
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/assistant/tutorial/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	public void test100Positive(final int tutorialIndex, final String code, final String title, final String abstracts, final String goals, final String course, final String estimatedTime) {
-//
-//		super.signIn("assistant4", "assistant4");
-//
-//		super.clickOnMenu("Assistant", "Tutorial List");
-//		super.sortListing(0, "asc");
-//		super.clickOnListingRecord(tutorialIndex);
-//		super.checkFormExists();
-//
-//		super.checkInputBoxHasValue("code", code);
-//		super.checkInputBoxHasValue("title", title);
-//		super.checkInputBoxHasValue("abstracts", abstracts);
-//		super.checkInputBoxHasValue("goals", goals);
-//		super.checkInputBoxHasValue("course", course);
-//		super.checkInputBoxHasValue("estimatedTime", estimatedTime);
-//
-//		super.signOut();
-//	}
-//
-//	@Test
-//	public void test300Hacking() {
-//
-//		Collection<Tutorial> tutorials;
-//		String param;
-//
-//		tutorials = this.repository.findManyTutorialsByAssistantUsername("assistant3");
-//		for (final Tutorial tutorial : tutorials)
-//			if (tutorial.isDraftMode()) {
-//				param = String.format("id=%d", tutorial.getId());
-//
-//				super.checkLinkExists("Sign in");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//
-//				super.signIn("administrator", "administrator");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//				super.signOut();
-//
-//				super.signIn("assistant5", "assistant5");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//				super.signOut();
-//
-//				super.signIn("lecturer1", "lecturer1");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//				super.signOut();
-//
-//				super.signIn("student1", "student1");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//				super.signOut();
-//
-//				super.signIn("company1", "company1");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//				super.signOut();
-//
-//				super.signIn("auditor1", "auditor1");
-//				super.request("/assistant/tutorial/show", param);
-//				super.checkPanicExists();
-//				super.signOut();
-//			}
-//	}
-//
-//}
+/*
+ * EmployerJobShowTest.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.testing.assistant.tutorial;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.Tutorial;
+import acme.testing.TestHarness;
+
+public class AssistantTutorialShowTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AssistantTutorialTestRepository repository;
+
+	// Test data --------------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int tutorialIndex, final String code, final String title, final String abstracts, final String goals, final String course, final String estimatedTime) {
+
+		super.signIn("assistant4", "assistant4");
+
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(tutorialIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("abstracts", abstracts);
+		super.checkInputBoxHasValue("goals", goals);
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("estimatedTime", estimatedTime);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+
+		Collection<Tutorial> tutorials;
+		String param;
+
+		tutorials = this.repository.findManyTutorialsByAssistantUsername("assistant3");
+		for (final Tutorial tutorial : tutorials)
+			if (tutorial.isDraftMode()) {
+				param = String.format("id=%d", tutorial.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+
+				super.signIn("administrator", "administrator");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("assistant5", "assistant5");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("lecturer1", "lecturer1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("student1", "student1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("company1", "company1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("auditor1", "auditor1");
+				super.request("/assistant/tutorial/show", param);
+				super.checkPanicExists();
+				super.signOut();
+			}
+	}
+
+}
--- src/test/java/acme/testing/lecturer/course/LecturerCoursePublishTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCoursePublishTest.java
@@ -1,147 +1,147 @@
-/*
- * EmployerJobPublishTest.java
- *
- * Copyright (C) 2012-2023 Rafael Corchuelo.
- *
- * In keeping with the traditional purpose of furthering education and research, it is
- * the policy of the copyright owner to permit non-commercial use and redistribution of
- * this software. It has been tested carefully, but it is not guaranteed for any particular
- * purposes. The copyright owner does not offer any warranties or representations, nor do
- * they accept any liabilities with respect to them.
- */
-
-package acme.testing.lecturer.course;
-
-import java.util.Collection;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import acme.entities.Course;
-import acme.testing.TestHarness;
-
-public class LecturerCoursePublishTest extends TestHarness {
-
-	// Internal data ----------------------------------------------------------
-
-	@Autowired
-	protected LecturerCourseRepositoryTest repository;
-
-	// Test methods -----------------------------------------------------------
-
-
-	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/course/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int courseIndex, final String code) {
-
-		super.signIn("lecturer9", "lecturer9");
-
-		super.clickOnMenu("Lecturer", "List of courses");
-		super.checkListingExists();
-		super.sortListing(0, "asc");
-		super.checkColumnHasValue(courseIndex, 0, code);
-
-		super.clickOnListingRecord(courseIndex);
-		super.checkFormExists();
-		super.clickOnSubmit("Publish");
-		super.checkNotErrorsExist();
-
-		super.signOut();
-	}
-
-	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/course/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test200Negative(final int courseIndex, final String code) {
-
-		super.signIn("lecturer4", "lecturer4");
-
-		super.clickOnMenu("Lecturer", "List of courses");
-		super.checkListingExists();
-		super.sortListing(0, "asc");
-
-		super.checkColumnHasValue(courseIndex, 0, code);
-		super.clickOnListingRecord(courseIndex);
-		super.checkFormExists();
-		super.clickOnSubmit("Publish");
-		super.checkErrorsExist();
-
-		super.signOut();
-	}
-
-	@Test
-	public void test300Hacking() {
-
-		Collection<Course> courses;
-		String params;
-
-		courses = this.repository.findManyCoursesByLecturerUsername("lecturer2");
-		for (final Course course : courses)
-			if (course.isDraftMode()) {
-				params = String.format("id=%d", course.getId());
-
-				super.checkLinkExists("Sign in");
-				super.request("/lecturer/course/publish", params);
-				super.checkPanicExists();
-
-				super.signIn("administrator", "administrator");
-				super.request("/lecturer/course/publish", params);
-				super.checkPanicExists();
-				super.signOut();
-
-				super.signIn("assistant1", "assistant1");
-				super.request("/lecturer/course/publish", params);
-				super.checkPanicExists();
-				super.signOut();
-
-				super.signIn("student1", "student1");
-				super.request("/lecturer/course/publish", params);
-				super.checkPanicExists();
-				super.signOut();
-
-				super.signIn("company1", "company1");
-				super.request("/lecturer/course/publish", params);
-				super.checkPanicExists();
-				super.signOut();
-
-				super.signIn("auditor1", "auditor1");
-				super.request("/lecturer/course/publish", params);
-				super.checkPanicExists();
-				super.signOut();
-			}
-	}
-
-	@Test
-	public void test301Hacking() {
-
-		Collection<Course> courses;
-		String params;
-
-		super.signIn("lecturer1", "lecturer1");
-		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
-		for (final Course course : courses)
-			if (!course.isDraftMode()) {
-				params = String.format("id=%d", course.getId());
-				super.request("/lecturer/course/publish", params);
-			}
-		super.signOut();
-	}
-
-	@Test
-	public void test302Hacking() {
-
-		Collection<Course> courses;
-		String params;
-
-		super.signIn("lecturer1", "lecturer1");
-		courses = this.repository.findManyCoursesByLecturerUsername("lecturer2");
-		for (final Course course : courses) {
-			params = String.format("id=%d", course.getId());
-			super.request("/lecturer/course/publish", params);
-
-		}
-		super.signOut();
-	}
-
-}
+///*
+// * EmployerJobPublishTest.java
+// *
+// * Copyright (C) 2012-2023 Rafael Corchuelo.
+// *
+// * In keeping with the traditional purpose of furthering education and research, it is
+// * the policy of the copyright owner to permit non-commercial use and redistribution of
+// * this software. It has been tested carefully, but it is not guaranteed for any particular
+// * purposes. The copyright owner does not offer any warranties or representations, nor do
+// * they accept any liabilities with respect to them.
+// */
+//
+//package acme.testing.lecturer.course;
+//
+//import java.util.Collection;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvFileSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import acme.entities.Course;
+//import acme.testing.TestHarness;
+//
+//public class LecturerCoursePublishTest extends TestHarness {
+//
+//	// Internal data ----------------------------------------------------------
+//
+//	@Autowired
+//	protected LecturerCourseRepositoryTest repository;
+//
+//	// Test methods -----------------------------------------------------------
+//
+//
+//	@ParameterizedTest
+//	@CsvFileSource(resources = "/lecturer/course/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+//	public void test100Positive(final int courseIndex, final String code) {
+//
+//		super.signIn("lecturer9", "lecturer9");
+//
+//		super.clickOnMenu("Lecturer", "List of courses");
+//		super.checkListingExists();
+//		super.sortListing(0, "asc");
+//		super.checkColumnHasValue(courseIndex, 0, code);
+//
+//		super.clickOnListingRecord(courseIndex);
+//		super.checkFormExists();
+//		super.clickOnSubmit("Publish");
+//		super.checkNotErrorsExist();
+//
+//		super.signOut();
+//	}
+//
+//	@ParameterizedTest
+//	@CsvFileSource(resources = "/lecturer/course/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+//	public void test200Negative(final int courseIndex, final String code) {
+//
+//		super.signIn("lecturer4", "lecturer4");
+//
+//		super.clickOnMenu("Lecturer", "List of courses");
+//		super.checkListingExists();
+//		super.sortListing(0, "asc");
+//
+//		super.checkColumnHasValue(courseIndex, 0, code);
+//		super.clickOnListingRecord(courseIndex);
+//		super.checkFormExists();
+//		super.clickOnSubmit("Publish");
+//		super.checkErrorsExist();
+//
+//		super.signOut();
+//	}
+//
+//	@Test
+//	public void test300Hacking() {
+//
+//		Collection<Course> courses;
+//		String params;
+//
+//		courses = this.repository.findManyCoursesByLecturerUsername("lecturer2");
+//		for (final Course course : courses)
+//			if (course.isDraftMode()) {
+//				params = String.format("id=%d", course.getId());
+//
+//				super.checkLinkExists("Sign in");
+//				super.request("/lecturer/course/publish", params);
+//				super.checkPanicExists();
+//
+//				super.signIn("administrator", "administrator");
+//				super.request("/lecturer/course/publish", params);
+//				super.checkPanicExists();
+//				super.signOut();
+//
+//				super.signIn("assistant1", "assistant1");
+//				super.request("/lecturer/course/publish", params);
+//				super.checkPanicExists();
+//				super.signOut();
+//
+//				super.signIn("student1", "student1");
+//				super.request("/lecturer/course/publish", params);
+//				super.checkPanicExists();
+//				super.signOut();
+//
+//				super.signIn("company1", "company1");
+//				super.request("/lecturer/course/publish", params);
+//				super.checkPanicExists();
+//				super.signOut();
+//
+//				super.signIn("auditor1", "auditor1");
+//				super.request("/lecturer/course/publish", params);
+//				super.checkPanicExists();
+//				super.signOut();
+//			}
+//	}
+//
+//	@Test
+//	public void test301Hacking() {
+//
+//		Collection<Course> courses;
+//		String params;
+//
+//		super.signIn("lecturer1", "lecturer1");
+//		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
+//		for (final Course course : courses)
+//			if (!course.isDraftMode()) {
+//				params = String.format("id=%d", course.getId());
+//				super.request("/lecturer/course/publish", params);
+//			}
+//		super.signOut();
+//	}
+//
+//	@Test
+//	public void test302Hacking() {
+//
+//		Collection<Course> courses;
+//		String params;
+//
+//		super.signIn("lecturer1", "lecturer1");
+//		courses = this.repository.findManyCoursesByLecturerUsername("lecturer2");
+//		for (final Course course : courses) {
+//			params = String.format("id=%d", course.getId());
+//			super.request("/lecturer/course/publish", params);
+//
+//		}
+//		super.signOut();
+//	}
+//
+//}
